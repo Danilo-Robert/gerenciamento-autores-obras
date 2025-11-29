e9b494bf7f7fb0b5d7df829127f23d13c1f56219
Danilo <99danilorobert@gmail.com>
2025-11-28 22:41:45 -0300

    primeiro commit

---
+++ src/main/java/com/danilo/gerenciamento_autores_obras/GerenciamentoAutoresObrasApplication.java
@@ -0,0 +1,13 @@
+package com.danilo.gerenciamento_autores_obras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAutoresObrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoAutoresObrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilo/gerenciamento_autores_obras/GerenciamentoAutoresObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilo.gerenciamento_autores_obras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoAutoresObrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
